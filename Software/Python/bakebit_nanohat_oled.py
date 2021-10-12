61ef6db1e4352de1caef3dd42b558d9405993402
Eugene Golovan <eugene.golovan@myntelligence.com>
2021-10-12 21:05:08 +0300

    Customized for pihole

--- Software/Python/bakebit_nanohat_oled.py
+++ Software/Python/bakebit_nanohat_oled.py
@@ -46,6 +46,9 @@ import threading
 import signal
 import os
 import socket
+import fcntl
+import struct
+import datetime
 
 global width
 width=128
@@ -58,6 +61,24 @@ global pageIndex
 pageIndex=0
 global showPageIndicator
 showPageIndicator=False
+
+global pageSleep
+pageSleep=120
+global pageSleepCountdown
+pageSleepCountdown=pageSleep
+
+global disableTimeSeconds
+disableTimeSeconds=900
+global disableCounter
+disableCounter=0
+
+global status
+status = "\"enabled\""
+
+global enabledMarkerShownSeconds
+enabledMarkerShownSeconds=5
+global enabledCounter
+enabledCounter=0
 
 oled.init()  #initialze SEEED OLED display
 oled.setNormalDisplay()      #Set display to normal mode (i.e non-inverse mode)
@@ -84,6 +105,14 @@ font11 = ImageFont.truetype('DejaVuSansMono.ttf', 11);
 global lock
 lock = threading.Lock()
 
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
@@ -95,6 +124,12 @@ def get_ip():
     finally:
         s.close()
     return IP
+
+def to_float(value):
+  try:
+    return '{:.2f}'.format(round(float(value), 2))
+  except:
+    return value
 
 def draw_page():
     global drawing
@@ -112,6 +147,12 @@ def draw_page():
     global width
     global height
     global lock
+    global pageSleepCountdown
+    global disableTimeSeconds
+    global disableCounter
+    global status
+    global enabledMarkerShownSeconds
+    global enabledCounter
 
     lock.acquire()
     is_drawing = drawing
@@ -120,6 +161,20 @@ def draw_page():
 
     if is_drawing:
         return
+
+    if disableCounter > 0:
+        disableCounter = disableCounter -1
+
+    #if the countdown is zero we should be sleeping (blank the display to reduce screenburn)
+    if pageSleepCountdown == 1:
+        oled.clearDisplay()
+        pageSleepCountdown = pageSleepCountdown - 1
+        return
+
+    if pageSleepCountdown == 0:
+        return
+
+    pageSleepCountdown = pageSleepCountdown - 1
 
     lock.acquire()
     drawing = True
@@ -141,21 +196,48 @@ def draw_page():
             dotTop=dotTop+dotWidth+dotPadding
 
     if page_index==0:
-        text = time.strftime("%A")
-        draw.text((2,2),text,font=font14,fill=255)
-        text = time.strftime("%e %b %Y")
-        draw.text((2,18),text,font=font14,fill=255)
-        text = time.strftime("%X")
-        draw.text((2,40),text,font=fontb24,fill=255)
-    elif page_index==1:
+        # text = time.strftime("%A")
+        # draw.text((2,2),text,font=font14,fill=255)
+        # text = time.strftime("%e %b %Y")
+        # draw.text((2,18),text,font=font14,fill=255)
+        # text = time.strftime("%X")
+        # draw.text((2,40),text,font=fontb24,fill=255)
         # Draw some shapes.
         # First define some constants to allow easy resizing of shapes.
-        padding = 0
+        padding = 2
         top = padding
         bottom = height-padding
         # Move left to right keeping track of the current x position for drawing shapes.
         x = 0
-        IPAddress = get_ip()
+	    IPAddress = get_ip()
+        cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+        CPU = subprocess.check_output(cmd, shell = True )
+        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+        MemUsage = subprocess.check_output(cmd, shell = True )
+        cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+        Disk = subprocess.check_output(cmd, shell = True )
+        tempI = int(open('/sys/class/thermal/thermal_zone0/temp').read());
+        if tempI>1000:
+            tempI = tempI/1000
+        tempStr = "CPU TEMP: %sC" % str(tempI)
+
+        draw.text((x, top+2),       "IP: " + str(IPAddress),  font=smartFont, fill=255)
+        draw.text((x, top+2+12),    str(CPU), font=smartFont, fill=255)
+        draw.text((x, top+2+24),    str(MemUsage),  font=smartFont, fill=255)
+        draw.text((x, top+2+36),    str(Disk),  font=smartFont, fill=255)
+        draw.text((x, top+2+48),    tempStr,  font=smartFont, fill=255)
+    elif page_index==1:
+        # Draw some shapes.
+        # First define some constants to allow easy resizing of shapes.
+        padding = 2
+        top = padding
+        bottom = height-padding
+        # Move left to right keeping track of the current x position for drawing shapes.
+        x = 0
+        try:
+            IPAddress = get_ip_address('eth0')
+        except:
+            IPAddress = get_ip()
         cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
         CPU = subprocess.check_output(cmd, shell=True).decode('utf-8')
         cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
@@ -166,14 +248,24 @@ def draw_page():
         if tempI>1000:
             tempI = tempI/1000
         tempStr = "CPU TEMP: %sC" % str(tempI)
+        cmd = "curl -f -s http://127.0.0.1/admin/api.php | jq .dns_queries_today"
+        Queries = subprocess.check_output(cmd, shell = True ).strip()
+        cmd = "curl -f -s http://127.0.0.1/admin/api.php | jq .ads_blocked_today"
+        AdsToday = subprocess.check_output(cmd, shell = True ).strip()
+        cmd = "curl -f -s http://127.0.0.1/admin/api.php | jq .ads_percentage_today"
+        AdsPercentage = to_float(subprocess.check_output(cmd, shell = True ).strip())
+        cmd = "curl -f -s http://127.0.0.1/admin/api.php | jq .clients_ever_seen"
+        ClientsEver = subprocess.check_output(cmd, shell = True ).strip()
+        cmd = "curl -f -s http://127.0.0.1/admin/api.php | jq .unique_clients"
+        ClientsUnique = subprocess.check_output(cmd, shell = True ).strip()
 
-        draw.text((x, top+5),       "IP: " + str(IPAddress),  font=smartFont, fill=255)
-        draw.text((x, top+5+12),    str(CPU), font=smartFont, fill=255)
-        draw.text((x, top+5+24),    str(MemUsage),  font=smartFont, fill=255)
-        draw.text((x, top+5+36),    str(Disk),  font=smartFont, fill=255)
-        draw.text((x, top+5+48),    tempStr,  font=smartFont, fill=255)
-    elif page_index==3: #shutdown -- no
-        draw.text((2, 2),  'Shutdown?',  font=fontb14, fill=255)
+        draw.text((x, top+2),       "IP: " + str(IPAddress),  font=smartFont, fill=255)
+        draw.text((x, top+2+12),    "Queries: " + str(Queries), font=smartFont, fill=255)
+        draw.text((x, top+2+24),    "Blocked: " + str(AdsToday),  font=smartFont, fill=255)
+        draw.text((x, top+2+36),    "Percent: " + str(AdsPercentage),  font=smartFont, fill=255)
+        draw.text((x, top+2+48),    "Clients: " + str(ClientsUnique),  font=smartFont, fill=255)
+    elif page_index==3: #Disable Pi-Hole for some senconds? -- no
+        draw.text((2, 2),  'Disable ' + str( int(float(disableTimeSeconds)/60) ) + 'min?',  font=fontb14, fill=255)
 
         draw.rectangle((2,20,width-4,20+16), outline=0, fill=0)
         draw.text((4, 22),  'Yes',  font=font11, fill=255)
@@ -181,8 +273,8 @@ def draw_page():
         draw.rectangle((2,38,width-4,38+16), outline=0, fill=255)
         draw.text((4, 40),  'No',  font=font11, fill=0)
 
-    elif page_index==4: #shutdown -- yes
-        draw.text((2, 2),  'Shutdown?',  font=fontb14, fill=255)
+    elif page_index==4: #Disable Pi-Hole for some senconds? -- yes
+        draw.text((2, 2),  'Disable ' + str( int(float(disableTimeSeconds)/60) ) + 'min?',  font=fontb14, fill=255)
 
         draw.rectangle((2,20,width-4,20+16), outline=0, fill=255)
         draw.text((4, 22),  'Yes',  font=font11, fill=0)
@@ -191,8 +283,24 @@ def draw_page():
         draw.text((4, 40),  'No',  font=font11, fill=255)
 
     elif page_index==5:
-        draw.text((2, 2),  'Shutting down',  font=fontb14, fill=255)
-        draw.text((2, 20),  'Please wait',  font=font11, fill=255)
+        cmd = "curl -f -s http://127.0.0.1/admin/api.php | jq .status"
+        status = subprocess.check_output(cmd, shell = True ).strip()
+        if str(status) == "\"disabled\"":
+            enabledCounter = 0
+            draw.text((2, 2),  'Disabled',  font=fontb14, fill=255)
+            if disableCounter > 0:
+                draw.text((2, 20),  str(datetime.timedelta(seconds=disableCounter)),  font=fontb24, fill=255)
+                draw.rectangle((2,47,int(float(width-4)*(float(disableCounter)/float(disableTimeSeconds))),47+15), outline=0, fill=255)
+        elif str(status) == "\"enabled\"":
+            disableCounter = 0
+            draw.text((2, 2),  'Enabled',  font=fontb14, fill=255)
+            if enabledCounter < enabledMarkerShownSeconds:
+                enabledCounter = enabledCounter + 1
+            else:
+                enabledCounter = 0
+                update_page_index(1)
+        else:
+            draw.text((2, 2),  'Disabled',  font=fontb14, fill=255)
 
     oled.drawImage(image)
 
@@ -201,7 +309,7 @@ def draw_page():
     lock.release()
 
 
-def is_showing_power_msgbox():
+def is_showing_disable_msgbox():
     global pageIndex
     lock.acquire()
     page_index = pageIndex
@@ -209,7 +317,6 @@ def is_showing_power_msgbox():
     if page_index==3 or page_index==4:
         return True
     return False
-
 
 def update_page_index(pi):
     global pageIndex
@@ -219,17 +326,29 @@ def update_page_index(pi):
 
 def receive_signal(signum, stack):
     global pageIndex
+    global pageSleepCountdown
+    global pageSleep
+    global disableTimeSeconds
+    global disableCounter
+    global status
+
+    if pageSleepCountdown == 0:
+        image1 = Image.open('pihole.png').convert('1')
+        oled.drawImage(image1)
+        time.sleep(0.5)
+
+    pageSleepCountdown = pageSleep #user pressed a button, reset the sleep counter
 
     lock.acquire()
     page_index = pageIndex
     lock.release()
 
-    if page_index==5:
-        return
+    #if page_index==5:
+    #    return
 
     if signum == signal.SIGUSR1:
-        print('K1 pressed')
-        if is_showing_power_msgbox():
+        print ('K1 pressed')
+        if is_showing_disable_msgbox():
             if page_index==3:
                 update_page_index(4)
             else:
@@ -238,35 +357,44 @@ def receive_signal(signum, stack):
         else:
             pageIndex=0
             draw_page()
-        print('K1 released')
+        print ('K1 released')
 
     if signum == signal.SIGUSR2:
-        print('K2 pressed')
-        if is_showing_power_msgbox():
+        print ('K2 pressed')
+        if is_showing_disable_msgbox():
             if page_index==4:
+                cmd = "curl -f -s \"http://127.0.0.1/admin/api.php?disable=" + str(disableTimeSeconds) + "&auth=$(docker exec -i pihole grep -oPi \"(?<=WEBPASSWORD\=).+\" /etc/pihole/setupVars.conf)\" | jq .status"
+                status = subprocess.check_output(cmd, shell = True )
+                lock.acquire()
+                disableCounter=disableTimeSeconds
+                lock.release()
                 update_page_index(5)
                 draw_page()
-
             else:
                 update_page_index(0)
                 draw_page()
         else:
             update_page_index(1)
             draw_page()
-        print('K2 released')
+        print ('K2 released')
 
     if signum == signal.SIGALRM:
-        print('K3 pressed')
-        if is_showing_power_msgbox():
+        print 'K3 pressed'
+        cmd = "curl -f -s http://127.0.0.1/admin/api.php | jq .status"
+        status = subprocess.check_output(cmd, shell = True ).strip()
+        if str(status) == "\"disabled\"":
+            enabledCounter = 0
+            update_page_index(5)
+            draw_page()
+        elif is_showing_disable_msgbox():
             update_page_index(0)
             draw_page()
         else:
             update_page_index(3)
             draw_page()
-        print('K3 released')
 
 
-image0 = Image.open('friendllyelec.png').convert('1')
+image0 = Image.open('pihole.png').convert('1')
 oled.drawImage(image0)
 time.sleep(2)
 
@@ -282,28 +410,7 @@ while True:
         page_index = pageIndex
         lock.release()
 
-        if page_index==5:
-            time.sleep(2)
-            while True:
-                lock.acquire()
-                is_drawing = drawing
-                lock.release()
-                if not is_drawing:
-                    lock.acquire()
-                    drawing = True
-                    lock.release()
-                    oled.clearDisplay()
-                    break
-                else:
-                    time.sleep(.1)
-                    continue
-            time.sleep(1)
-            os.system('systemctl poweroff')
-            break
-        elif page_index==1:
-            time.sleep(1)
-        else:
-            time.sleep(0.2)
+        time.sleep(1)
     except KeyboardInterrupt:
         break
     except IOError:
